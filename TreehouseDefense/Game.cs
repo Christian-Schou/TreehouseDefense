724bd5e4455161e13d35fab44e2ace12a406dc99
Christian Schou <christian.schou17@gmail.com>
2017-12-04 08:45:13 +0100

    Created the project and added some functionality in Point.cs

---
+++ TreehouseDefense/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreehouseDefense
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            Map map = new Map(8, 5);
+
+            try
+            {
+                MapLocation mapLocation = new MapLocation(20, 20, map);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
