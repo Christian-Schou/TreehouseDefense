ad6009beeda0661ce5f77317d54c20b387a113eb
Christian Schou <christian.schou17@gmail.com>
2017-12-14 17:44:37 +0100

    Added tower.cs

--- TreehouseDefense/Tower.cs
+++ TreehouseDefense/Tower.cs
@@ -8,5 +8,21 @@ namespace TreehouseDefense
 {
     class Tower
     {
+        private readonly MapLocation _location;
+
+        public Tower(MapLocation location)
+        {
+            _location = location;
+        }
+
+        public void FireOnInvaders(Invader[] invaders)
+        {
+            for (int index = 0; index < invaders.Length; index++)
+            {
+                Invader invader = invaders[index];
+                //Do stuff with invader
+            }
+        }
+
     }
 }
